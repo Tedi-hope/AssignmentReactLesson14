d56ab0901413b2fc351deda86bfdf7c24226c0db
Tedros Habtu <amhopefule07@gmail.com>
2024-12-05 11:41:17 +0300

    Project set up and components created

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>Features</div>
+  )
+}
+
+export default Footer
---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+    Company Name
+    </div>
+  )
+}
+
+export default Header
